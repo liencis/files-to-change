7bf472d17fe0d911620496907f1e82a17d1a7aa4
Liene Zegele <lienezegele@gmail.com>
2025-11-08 17:32:53 +0200

    Create lemon-menu.js

---
+++ lemon-menu.js
@@ -0,0 +1,132 @@
+{
+  "menu": [
+    {
+      "id": 1,
+      "title": "Spinach Artichoke Dip",
+      "price": "10",
+      "description": "",
+      "image": "",
+      "category": "Appetizers"
+    },
+    {
+      "id": 2,
+      "title": "Hummus",
+      "price": "10",
+      "description": "",
+      "image": "",
+      "category": "Appetizers"
+    },
+    {
+      "id": 3,
+      "title": "Fried Calamari Rings",
+      "price": "51",
+      "description": "",
+      "image": "",
+      "category": "Appetizers"
+    },
+    {
+      "id": 4,
+      "title": "Fried Mushroom",
+      "price": "12",
+      "description": "",
+      "image": "",
+      "category": "Appetizers"
+    },
+    {
+      "id": 5,
+      "title": "Greek",
+      "price": "7",
+      "description": "",
+      "image": "",
+      "category": "Salads"
+    },
+    {
+      "id": 6,
+      "title": "Caesar",
+      "price": "7",
+      "description": "",
+      "image": "",
+      "category": "Salads"
+    },
+    {
+      "id": 7,
+      "title": "Mediterranean Tuna Salad",
+      "price": "10",
+      "description": "",
+      "image": "",
+      "category": "Salads"
+    },
+    {
+      "id": 8,
+      "title": "Grilled Chicken Salad",
+      "price": "12",
+      "description": "",
+      "image": "",
+      "category": "Salads"
+    },
+    {
+      "id": 9,
+      "title": "Water",
+      "price": "3",
+      "description": "",
+      "image": "",
+      "category": "Beverages"
+    },
+    {
+      "id": 10,
+      "title": "Coke",
+      "price": "3",
+      "description": "",
+      "image": "",
+      "category": "Beverages"
+    },
+    {
+      "id": 11,
+      "title": "Beer",
+      "price": "7",
+      "description": "",
+      "image": "",
+      "category": "Beverages"
+    },
+    {
+      "id": 12,
+      "title": "Iced Tea",
+      "price": "3",
+      "description": "",
+      "image": "",
+      "category": "Beverages"
+    }
+    {
+      "id": 13,
+      "name": "Bruschetta",
+      "price": "7.99",
+      "description": "Delicious grilled bread rubbed with garlic and topped with olive oil and salt. Our Bruschetta includes tomato and cheese.",
+      "image": "bruschetta.jpg",
+      "category": "Appetizers"
+    },
+    {
+      "id": 14,
+      "name": "Grilled Fish",
+      "price": "20.00",
+      "description": "Fantastic grilled fish seasoned with salt.",
+      "image": "grilledFish.jpg",
+      "category": "Mains"
+    },
+    {
+      "id": 15,
+      "name": "Pasta",
+      "price": "6.99",
+      "description": "Delicious pasta for your delight.",
+      "image": "pasta.jpg",
+      "category": "Mains"      
+    },
+    {
+      "id": 16,
+      "name": "Lemon Dessert",
+      "price": "4.99",
+      "description": "You can't go wrong with this delicious lemon dessert!",
+      "image": "lemonDessert.jpg",
+      "category": "desserts"      
+    }
+  ]
+}
